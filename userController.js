1105e8124d80fda46807c5ff2f165b2dc361a9fd
Whited123 <123481645+Whited123@users.noreply.github.com>
2023-02-16 19:05:38 +0900

    Add files via upload

---
+++ userController.js
@@ -0,0 +1,3 @@
+export const join = (req, res) => res.send("구경 그만하고 조인해");
+export const edit = (req, res) => res.send("계정 정보 수정");
+export const remove = (req, res) => res.send("계삭ㅋㅋ");
---
+++ videoController.js
@@ -0,0 +1,3 @@
+export const trending = (req, res) => res.send("환영합니다.");
+export const watch = (req, res) => res.send("즐감");
+export const edit = (req, res) => res.send("수정");
