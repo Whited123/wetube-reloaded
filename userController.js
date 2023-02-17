51e1332591f4e69e5701d27fbf37c60b45dbc7ce
Whited123 <123481645+Whited123@users.noreply.github.com>
2023-02-17 15:51:58 +0900

    Add files via upload

--- userController.js
+++ userController.js
@@ -1,3 +1,6 @@
 export const join = (req, res) => res.send("구경 그만하고 조인해");
 export const edit = (req, res) => res.send("계정 정보 수정");
-export const remove = (req, res) => res.send("계삭ㅋㅋ");
+export const remove = (req, res) => res.send("계삭 ㅋㅋ");
+export const login = (req, res) => res.send("로그인 ㅋㅋ");
+export const logout = (req, res) => res.send("로그아웃 ㅋㅋ");
+export const see = (req, res) => res.send("보기");
--- videoController.js
+++ videoController.js
@@ -1,3 +1,6 @@
 export const trending = (req, res) => res.send("환영합니다.");
-export const watch = (req, res) => res.send("즐감");
+export const see = (req, res) => res.send("즐감");
 export const edit = (req, res) => res.send("수정");
+export const search = (req, res) => res.send("설치");
+export const upload = (req, res) => res.send("올리기");
+export const deleteVideo = (req, res) => res.send("내리기");
