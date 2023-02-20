ef9a3d43eabf6182f62d0f6345bb5f960ea849e9
Whited123 <123481645+Whited123@users.noreply.github.com>
2023-02-20 17:51:05 +0900

    Add files via upload

--- videoController.js
+++ videoController.js
@@ -1,9 +1,6 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { titlePage: "Home" });
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-  console.log(req.params);
-  return res.send("수정");
-};
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("설치");
 export const upload = (req, res) => res.send("올리기");
 export const deleteVideo = (req, res) => {
