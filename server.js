7f2055e8a561532ef233924818b5206735a5d223
Whited123 <123481645+Whited123@users.noreply.github.com>
2023-02-22 19:42:20 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
-import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
-const PORT = 4500;
-
-console.log(process.cwd());
+const PORT = 4400;
 
 const app = express();
 const logger = morgan("dev");
@@ -15,8 +13,8 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use("/", globalRouter);
-app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
--- videoController.js
+++ videoController.js
@@ -1,12 +1,39 @@
+let videos = [
+  {
+    title: "한 코딩 마스터가 해냈다. 누적 조회수 1억만 뷰의 극한의 화제작!!!",
+    rating: 2,
+    comments: 7,
+    createAt: "2일",
+    views: 169,
+    id: 1,
+  },
+  {
+    title:
+      "코딩이 벤치 120치는거 보다 쉬워요. 세상을 놀라게 한 최연소(26) 코딩 천재의 압도적인 강의",
+    rating: 3,
+    comments: 7,
+    createAt: "2일",
+    views: 169,
+    id: 2,
+  },
+  {
+    title: "왜 다들 내 깃허브 보고 화장실 가는 지 모르겠어.",
+    rating: 4.5,
+    comments: 7,
+    createAt: "2일",
+    views: 169,
+    id: 3,
+  },
+];
 export const trending = (req, res) => {
-  const videos = [];
   return res.render("home", { titlePage: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { titlePage: `Watching ${video.title}` }, video);
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("설치");
 export const upload = (req, res) => res.send("올리기");
-export const deleteVideo = (req, res) => {
-  console.log(req.params);
-  return res.send("내리기");
-};
+export const deleteVideo = (req, res) => res.send("내리기");
