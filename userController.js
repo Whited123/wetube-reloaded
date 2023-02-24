51c437aae22f5d15332e33857098b31cf798aca0
Whited123 <123481645+Whited123@users.noreply.github.com>
2023-02-25 00:18:02 +0900

    Delete userController.js

--- userController.js
+++
@@ -1,6 +0,0 @@
-export const join = (req, res) => res.send("구경 그만하고 조인해");
-export const edit = (req, res) => res.send("계정 정보 수정");
-export const remove = (req, res) => res.send("계삭 ㅋㅋ");
-export const login = (req, res) => res.send("로그인 ㅋㅋ");
-export const logout = (req, res) => res.send("로그아웃 ㅋㅋ");
-export const see = (req, res) => res.send("보기");
