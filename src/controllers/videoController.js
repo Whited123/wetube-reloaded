34627411d52a78cf2c2d269715504448fae39090
kim dong young <dudtodtls24@gmail.com>
2023-02-25 14:49:27 +0900

    #윾투브 챌린지 2주차주말 예습
    #6-11

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,47 +1,20 @@
-let videos = [
-  {
-    title: "한 코딩 마스터가 해냈다. 누적 조회수 1억만 뷰의 극한의 화제작!!!",
-    rating: 2,
-    comments: 7,
-    createAt: "2일",
-    views: 169,
-    id: 1,
-  },
-  {
-    title:
-      "코딩이 벤치 120치는거 보다 쉬워요. 세상을 놀라게 한 최연소(26) 코딩 천재의 압도적인 강의",
-    rating: 3,
-    comments: 7,
-    createAt: "2일",
-    views: 169,
-    id: 2,
-  },
-  {
-    title: "왜 다들 내 깃허브 보고 화장실 가는 지 모르겠어.",
-    rating: 4.5,
-    comments: 7,
-    createAt: "2일",
-    views: 169,
-    id: 3,
-  },
-];
-export const trending = (req, res) => {
-  return res.render("home", { titlePage: "Home", videos });
+import Video from "../models/Video";
+
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {});
+  return res.render("home", { titlePage: "Home" });
 };
 export const watch = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("watch", { titlePage: `Watching: ${video.title}`, video });
+  return res.render("watch", { titlePage: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { titlePage: `Editing: ${video.title}`, video });
+  return res.render("edit", { titlePage: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -50,14 +23,5 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = (req, res) => {
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createAt: "방금",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
   return res.redirect("/");
 };
---
+++ src/init.js
@@ -0,0 +1,10 @@
+import "./db";
+import "./models/Video";
+import app from "./server";
+
+const PORT = 4400;
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
