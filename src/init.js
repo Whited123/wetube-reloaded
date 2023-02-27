39504f4ba4fdcccdc5cdec150caae581ec9fa5ac
kim dong young <dudtodtls24@gmail.com>
2023-02-27 20:48:13 +0900

    유투브 챌 11일차
    #6-27
    후............

--- src/init.js
+++ src/init.js
@@ -2,7 +2,7 @@ import "./db";
 import "./models/Video";
 import app from "./server";
 
-const PORT = 4400;
+const PORT = 4600;
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
