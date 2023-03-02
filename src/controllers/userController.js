294602ff00658903e38ec2c80504f096e141f489
kim dong young <dudtodtls24@gmail.com>
2023-03-02 15:51:01 +0900

    유툽챌 14일
    7-4 상태코드 브라우저한테 알려줘야 브라우저가 "아 이새기 오류떴구나"하고 안다!!!

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,14 +5,14 @@ export const postJoin = async (req, res) => {
   const { email, username, password, password2, name, location } = req.body;
   const pageTitle = "Join";
   if (password !== password2) {
-    res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "비밀번호가 일치하지 않습니다.",
     });
   }
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    res.render("join", {
+    return res.status(400).render("join", {
       pageTitle,
       errorMessage: "이 이메일/유저 네임은 이미 사용 중 입니다 !!!",
     });
