005fb769da2cb37506c86e12c748f8a2a9579f8b
kim dong young <dudtodtls24@gmail.com>
2023-03-02 18:21:10 +0900

    유툽 챌 14
    7-10 백엔도 세션 쿠키 로컬 던

---
+++ src/middlewares.js
@@ -0,0 +1,7 @@
+export const localsMiddleware = (req, res, next) => {
+  res.locals.loggedIn = Boolean(req.session.loggedIn);
+  res.locals.siteName = "웨튜브";
+  res.locals.loggedInUser = req.session.user;
+  console.log(res.locals);
+  next();
+};
