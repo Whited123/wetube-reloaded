ae1d3b1995a926ec91de5afee1b467a33900807b
kim dong young <dudtodtls24@gmail.com>
2023-03-08 16:14:24 +0900

    유툽 챌 4주차 수욜
    8-1 프로텍트 퍼블릭 미들웨어 만들기

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,6 +1,24 @@
+import { render, renderFile } from "pug";
+
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "웨튜브";
   res.locals.loggedInUser = req.session.user || {};
   next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
