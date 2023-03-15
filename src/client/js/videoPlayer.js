2836fd241606ccfd65c43bff3d5587389b648ecb
kim dong young <dudtodtls24@gmail.com>
2023-03-15 18:23:03 +0900

    유툽챌 5주차 수욜
    12-2 후....오타 와 에러 ... 최강자의 길 험난하기만 해

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -102,13 +102,15 @@ const handleMouseLeave = () => {
 
 const handleEnded = () => {
   const { id } = videoContainer.dataset;
-  fetch(`/api/videos/${id}/view`, { method: "POST" });
+  fetch(`/api/videos/${id}/view`, {
+    method: "POST",
+  });
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
-video.addEventListener("loadeddata", handleLoadedMetadata);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("ended", handleEnded);
 videoContainer.addEventListener("mousemove", handleMouseMove);
